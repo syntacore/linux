f45d3c5e6ff4e2ef2d2427141baeeb977b308504
Bin Meng <bmeng@tinylab.org>
2022-12-22 13:29:36 +0300

    serial: earlycon-arm-semihost: Move smh_putc() variants in respective arch's semihost.h
    Move smh_putc() variants in respective arch/*/include/asm/semihost.h,
    in preparation to add RISC-V support.
    
    Signed-off-by: Bin Meng <bmeng@tinylab.org>

--- drivers/tty/serial/earlycon-arm-semihost.c
+++ drivers/tty/serial/earlycon-arm-semihost.c
@@ -12,38 +12,17 @@
 #include <linux/init.h>
 #include <linux/serial_core.h>
 
-#ifdef CONFIG_THUMB2_KERNEL
-#define SEMIHOST_SWI	"0xab"
-#else
-#define SEMIHOST_SWI	"0x123456"
-#endif
+#include <asm/semihost.h>
 
-/*
- * Semihosting-based debug console
- */
-static void smh_putc(struct uart_port *port, int c)
-{
-#ifdef CONFIG_ARM64
-	asm volatile("mov  x1, %0\n"
-		     "mov  x0, #3\n"
-		     "hlt  0xf000\n"
-		     : : "r" (&c) : "x0", "x1", "memory");
-#else
-	asm volatile("mov  r1, %0\n"
-		     "mov  r0, #3\n"
-		     "svc  " SEMIHOST_SWI "\n"
-		     : : "r" (&c) : "r0", "r1", "memory");
-#endif
-}
-
-static void smh_write(struct console *con, const char *s, unsigned n)
+static void smh_write(struct console *con,
+		      const char *s, unsigned n)
 {
 	struct earlycon_device *dev = con->data;
 	uart_console_write(&dev->port, s, n, smh_putc);
 }
 
-static int
-__init early_smh_setup(struct earlycon_device *device, const char *opt)
+static int __init early_smh_setup(struct earlycon_device *device,
+				  const char *opt)
 {
 	device->con->write = smh_write;
 	return 0;
